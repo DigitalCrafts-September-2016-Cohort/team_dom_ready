7d30b95252d28b5bcbad7feb18d4a47ca2447c60
Dominic Zenon <dominiczenon@gmail.com>
2016-11-29 10:33:17 -0500

    added

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import logging
 import os
 
 logging.basicConfig(stream=sys.stderr)
-sys.path.dirname(os.path.realpath(__file__))
+path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, path)
 #the name server below is the name of your server.py file
 #if not, modify the name
